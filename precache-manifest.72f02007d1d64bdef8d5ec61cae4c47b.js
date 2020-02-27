85db3214ee444ef4a6a639625708242c4cba8ecf
bennisan86 <info@bennisan.be>
2020-02-27 11:59:30 +0100

    Updates

---
+++ precache-manifest.72f02007d1d64bdef8d5ec61cae4c47b.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a67d9b420fc9d770c9d6e2e516d0bab",
+    "url": "/portf_v2/index.html"
+  },
+  {
+    "revision": "09765e2850c2992bb54e",
+    "url": "/portf_v2/static/css/main.20f6b451.chunk.css"
+  },
+  {
+    "revision": "6b54bddd432a1b6c483e",
+    "url": "/portf_v2/static/js/2.1e03e4d6.chunk.js"
+  },
+  {
+    "revision": "2e7e5a466ae113f07513391c0542446b",
+    "url": "/portf_v2/static/js/2.1e03e4d6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "09765e2850c2992bb54e",
+    "url": "/portf_v2/static/js/main.4b6ef096.chunk.js"
+  },
+  {
+    "revision": "3743eee5a1ffd5f69133",
+    "url": "/portf_v2/static/js/runtime-main.0abb9cae.js"
+  },
+  {
+    "revision": "ef54147d425f1742633b0b457c0a7e01",
+    "url": "/portf_v2/static/media/Bennisan_CV.ef54147d.pdf"
+  },
+  {
+    "revision": "309acf3ba2b990cee49700a3ebb5cabd",
+    "url": "/portf_v2/static/media/IMG_1051.309acf3b.png"
+  },
+  {
+    "revision": "b94572f7b855401d5a73094405bb8526",
+    "url": "/portf_v2/static/media/backarrow.b94572f7.svg"
+  },
+  {
+    "revision": "c3a869f652098bd4cab2996dd5bf3078",
+    "url": "/portf_v2/static/media/github.c3a869f6.svg"
+  },
+  {
+    "revision": "d3117811e18bf5d71d871d8ea012f87e",
+    "url": "/portf_v2/static/media/github_white.d3117811.svg"
+  },
+  {
+    "revision": "db3b25fcaa80812e408338ca79f2f555",
+    "url": "/portf_v2/static/media/logo.db3b25fc.svg"
+  },
+  {
+    "revision": "948af40dd619a4b4ff535aa46cfb8fe9",
+    "url": "/portf_v2/static/media/logo_white.948af40d.svg"
+  },
+  {
+    "revision": "ad688b66d3527410cb79bf1632fa7769",
+    "url": "/portf_v2/static/media/mail.ad688b66.svg"
+  },
+  {
+    "revision": "4545c76df98bd7b43dcf4f900f44544c",
+    "url": "/portf_v2/static/media/twitter.4545c76d.svg"
+  },
+  {
+    "revision": "7bc45df99162894078b6ae696ca02e44",
+    "url": "/portf_v2/static/media/twitter_white.7bc45df9.svg"
+  }
+]);
